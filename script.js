412b6e945c825357ccf616ecbb7679d2f5ddb68c
cina2012isi-sys <cina2012isi@gmail.com>
2026-08-12 01:22:03 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "ТВОЙ_PROJECT_URL";
-const SUPABASE_KEY = "ТВОЙ_PUBLISHABLE_KEY";
+const SUPABASE_URL = "https://ftwfmoqnwhnrqbbcqjpl.supabase.co/rest/v1/_PROJECT_URL";
+const SUPABASE_KEY = "sb_publishable_NLX3HgBrQjAvmUUI9Jjx6A_9glPH1vq_PUBLISHABLE_KEY";
 
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
