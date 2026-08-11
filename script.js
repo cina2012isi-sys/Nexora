bd437ee2a2bd93b5ba628c26baf52c884c60b055
cina2012isi-sys <cina2012isi@gmail.com>
2026-08-12 01:17:36 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://ftwfmoqnwhnrqbbcqjpl.supabase.co/rest/v1/_PROJECT_URL";
-const SUPABASE_KEY = "sb_publishable_NLX3HgBrQjAvmUUI9Jjx6A_9glPH1vq_PUBLISHABLE_KEY";
+const SUPABASE_URL = "ТВОЙ_PROJECT_URL";
+const SUPABASE_KEY = "ТВОЙ_PUBLISHABLE_KEY";
 
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
@@ -7,80 +7,19 @@ const supabase = window.supabase.createClient(
 );
 
 
-// Загрузка сообщений
-async function loadPosts() {
+// Realtime
+supabase
+    .channel("messages-channel")
+    .on(
+        "postgres_changes",
+        {
+            event: "*",
+            schema: "public",
+            table: "messages"
+        },
+        () => {
+            loadPosts();
+        }
+    )
+    .subscribe();
 
-    const { data, error } = await supabase
-        .from("messages")
-        .select("*")
-        .order("created_at", { ascending: false });
-
-    if (error) {
-        console.error("Ошибка загрузки:", error);
-        return;
-    }
-
-    const posts = document.getElementById("posts");
-    posts.innerHTML = "";
-
-    data.forEach(post => {
-        showPost(post);
-    });
-}
-
-
-// Добавление сообщения
-async function addPost() {
-
-    const input = document.getElementById("messageInput");
-
-    const text = input.value.trim();
-
-    if (!text) {
-        return;
-    }
-
-    const { error } = await supabase
-        .from("messages")
-        .insert({
-            author: "Аноним",
-            text: text
-        });
-
-    if (error) {
-        console.error("Ошибка отправки:", error);
-        alert("Не удалось отправить сообщение");
-        return;
-    }
-
-    input.value = "";
-
-    loadPosts();
-}
-
-
-// Отображение сообщения
-function showPost(post) {
-
-    const posts = document.getElementById("posts");
-
-    const element = document.createElement("div");
-
-    element.className = "post";
-
-    element.innerHTML = `
-        <div class="post-header">
-            <b>${post.author}</b>
-        </div>
-
-        <div class="post-text">
-            ${post.text}
-        </div>
-    `;
-
-    posts.appendChild(element);
-}
-
-
-// Загружаем сообщения при открытии
-loadPosts();
